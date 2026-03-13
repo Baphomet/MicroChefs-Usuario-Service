fa051dfb3cd43c233be8773a7e4bfaadce0ac2af
Baphomet <fgrochovski@gmail.com>
2026-03-13 16:50:36 -0300

    feat: implementa EnderecoService com operações CRUD

--- obj/Debug/net8.0/ClienteService.AssemblyInfo.cs
+++ obj/Debug/net8.0/ClienteService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClienteService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd2f6aa5ac3cd3505cea703a4fa14747b9d69ea4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11baa61d630f526c3d60d30ef01e1f3b96504e5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClienteService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClienteService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
