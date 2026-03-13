5fc68511d18bb63ad09903ca0411651608de6d1a
Felipe Grochovski <fgrochovski@gmail.com>
2026-03-13 20:02:13 -0300

    feat: adiciona ClienteController

--- obj/Debug/net8.0/ClienteService.AssemblyInfo.cs
+++ obj/Debug/net8.0/ClienteService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClienteService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47cf47b0113530a19eebf2e4390387e9af9a68c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4df04cf504d12547baa8a2238997f15488aa4abe")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClienteService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClienteService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
