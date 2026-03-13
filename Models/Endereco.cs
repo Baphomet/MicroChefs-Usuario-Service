84305e8e5f41514730f8f9ef2edb5444ce21be29
Baphomet <fgrochovski@gmail.com>
2026-03-13 16:11:05 -0300

    feat: adicionar propriedades da classe Endereco

--- Models/Endereco.cs
+++ Models/Endereco.cs
@@ -1,6 +1,28 @@
-﻿namespace ClienteService.Models
+﻿using System.Text.Json.Serialization;
+
+namespace ClienteService.Models
 {
     public class Endereco
     {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string Rua { get; set; }
+
+        public string Numero { get; set; }
+
+        public string Complemento { get; set; }
+
+        public string Bairro { get; set; }
+
+        public string Cidade { get; set; }
+
+        public string Estado { get; set; }
+
+        public string Cep { get; set; }
+
+        public Guid ClienteId { get; set; }
+
+        [JsonIgnore]
+        public Cliente Cliente { get; set; }
     }
 }
