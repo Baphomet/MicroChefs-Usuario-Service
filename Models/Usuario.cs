8fc360d8460c20cb10da43db97879e8cdf6d05a2
Baphomet <fgrochovski@gmail.com>
2026-03-13 16:16:09 -0300

    feat: adicionar propriedades da classe Usuario

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -1,6 +1,24 @@
-﻿namespace ClienteService.Models
+﻿using System.Text.Json.Serialization;
+
+namespace ClienteService.Models
 {
     public class Usuario
     {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public string SenhaHash { get; set; }
+
+        public string SenhaSalt { get; set; }
+
+        public string Role { get; set; }
+
+        public DateTime DataCriacao { get; set; } = DateTime.UtcNow;
+
+        [JsonIgnore]
+        public Cliente Cliente { get; set; }
     }
 }
