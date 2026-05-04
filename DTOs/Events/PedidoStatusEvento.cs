13eeb00c5f23fba6ac8d8a998f180bc3e91ec0d1
Baphomet <fgrochovski@gmail.com>
2026-05-03 21:31:39 -0300

    feat: adiciona DTO PedidoStatusEvento

---
+++ DTOs/Events/PedidoStatusEvento.cs
@@ -0,0 +1,9 @@
+﻿namespace ClienteService.DTOs.Eventos
+{
+    public class PedidoStatusEvento
+    {
+        public long Id { get; set; }
+        public long UsuarioId { get; set; }
+        public string StatusPedido { get; set; }
+    }
+}
