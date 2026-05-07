94215cb48db38d5e60fa760b642aec224733dd97
Baphomet <fgrochovski@gmail.com>
2026-05-06 22:36:34 -0300

    feat: adiciona DTO de suporte para mensagens DLQ

---
+++ DTOs/Events/DLQSupportDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace ClienteService.DTOs.Events
+{
+    public record DLQSupportDTO
+    (
+        string TipoMensagem,
+        string FilaDeOrigem,
+        string TipoErro,
+        string MensagemDeErro,
+        string MensagemOriginal,
+        DateTime TimeStamp
+    );
+}
