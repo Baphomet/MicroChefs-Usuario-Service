91ef8b26fadd918ee9290a970a32b30b81d3113c
Baphomet <fgrochovski@gmail.com>
2026-05-07 22:52:09 -0300

    feat: ajuste na DLQSupportDTO pra utilizar jsonproperty

--- DTOs/Events/DLQSupportDTO.cs
+++ DTOs/Events/DLQSupportDTO.cs
@@ -1,12 +1,14 @@
-﻿namespace ClienteService.DTOs.Events
+﻿using System.Text.Json.Serialization;
+
+namespace ClienteService.DTOs.Events
 {
     public record DLQSupportDTO
     (
-        string TipoMensagem,
-        string FilaDeOrigem,
-        string TipoErro,
-        string MensagemDeErro,
-        string MensagemOriginal,
-        DateTime TimeStamp
+        [property: JsonPropertyName("tipoMensagem")] string TipoMensagem,
+        [property: JsonPropertyName("filaDeOrigem")] string FilaDeOrigem,
+        [property: JsonPropertyName("tipoErro")] string TipoErro,
+        [property: JsonPropertyName("mensagemDeErro")] string MensagemDeErro,
+        [property: JsonPropertyName("mensagemOriginal")] string MensagemOriginal,
+        [property: JsonPropertyName("timestamp")] DateTime TimeStamp
     );
 }
